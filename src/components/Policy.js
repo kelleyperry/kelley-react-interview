4b7e7734f847e84b80ab5c34b4307b444804147f
Kelley Perry <kelley.perry@csaa.com>
2017-08-27 23:24:35 -0700

    added toggle

--- src/components/Policy.js
+++ src/components/Policy.js
@@ -1,44 +1,66 @@
 import React from 'react';
 
 export default class Policy extends React.Component {
-  render() {
-    let policyItem, policyHolderName;
-    policyItem = this.props.policies.filter((policy) =>
-        policy.type === this.props.policyType
-      ).map((policy) =>
-        <div className="policy-item" key={policy.policyNumber}>
-          <div className="policy-holder">Policy Holder: <span>{
-            policyHolderName = this.props.policyHolders.map(function(policyHolder) {
-              let policyHolderArr = [];
-              policyHolderArr.push(policyHolder);
-              for (let i = 0; i < policyHolderArr.length; i++) {
-                if (policyHolderArr[i].policies.includes(policy.policyNumber)) {
-                  return policyHolderArr[i].name;
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: []
+    }
+  }
+
+  toggleDetails (index) {
+    let selected = this.state.selected.slice();
+    selected[index] = !selected[index];
+    this.setState({ selected });
+  }
+
+  renderMappedPolicy () {
+    let { policyHolderName, policies, policyType, policyHolders } = this.props;
+    return policies.filter((policy) =>
+      policy.type === policyType
+    ).map((policy, i) =>
+      <div className="policy-item" key={i} onClick={() => this.toggleDetails(i)}>
+        <div className="policy-holder">Policy Holder: <span>
+            {
+            policyHolderName = policyHolders.map(function (policyHolder) {
+                let policyHolderArr = [];
+                policyHolderArr.push(policyHolder);
+                for (let i = 0; i < policyHolderArr.length; i++) {
+                  if (policyHolderArr[i].policies.includes(policy.policyNumber)) {
+                    return policyHolderArr[i].name;
+                  }
                 }
               }
-            }
-          )}</span></div>
-          <div className="policy-number">Policy Number: <span>{policy.policyNumber}</span></div>
-          <div className="policy-type">Policy Type: <span>{policy.type}</span></div>
-          <div className={`policy-${this.props.policyType === 'HOME' ?
-            'address' :
-            'vehicle'}`}>
-            <span>
-            {this.props.policyType === 'HOME' ?
-              ('Address: ' + policy.address) :
-              ('Vehicle: ' + policy.vehicle)}
-            </span>
-          </div>
-          <div className="policy-details">
-            <div className="policy-enrollment">Enrollment Date: <span>{policy.enrollmentDate}</span></div>
-            <div className="policy-amount-due">Amount Due: <span>${policy.amountDue}</span></div>
-          </div>
+            )}
+          </span>
         </div>
+        <div className="policy-number">Policy Number: <span>{policy.policyNumber}</span></div>
+        <div className="policy-type">Policy Type: <span>{policy.type}</span></div>
+        <div className={`policy-${policyType === 'HOME' ?
+          'address' :
+          'vehicle'}`}>
+              <span>
+              {policyType === 'HOME' ?
+                ('Address: ' + policy.address) :
+                ('Vehicle: ' + policy.vehicle)}
+              </span>
+        </div>
+        {(this.state.selected[i]) ? <div className="policy-details">
+          <div className="policy-enrollment">Enrollment Date: <span>{policy.enrollmentDate}</span></div>
+          <div className="policy-amount-due">Amount Due: <span>${policy.amountDue}</span></div>
+        </div> : null}
+      </div>
     )
+  }
+
+  render () {
     return (
       <div>
-        {policyItem}
+        {this.renderMappedPolicy()}
       </div>
     )
   }
 }
+
+
+
--- src/components/PolicyList.js
+++ src/components/PolicyList.js
@@ -4,18 +4,16 @@ import '../css/styles.css';
 import Policy from './Policy';
 
 export default class PolicyList extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-
     this.state = {
       policies: [],
       policyHolders: [],
-      policyType: 'HOME'
-
+      policyType: 'HOME',
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     getData().then((data) => {
       this.setState({
         policies: data.policies,
@@ -24,24 +22,26 @@ export default class PolicyList extends React.Component {
     });
   }
 
-  showHomePolicies() {
-    this.setState({policyType: 'HOME'});
+  showHomePolicies () {
+    this.setState({ policyType: 'HOME' });
   }
 
-  showAutoPolicies() {
-    this.setState({policyType: 'AUTO'});
+  showAutoPolicies () {
+    this.setState({ policyType: 'AUTO' });
   }
 
-  render() {
+  render () {
     const { policies, policyType, policyHolders } = this.state;
     return (
       <div>
-        <label><input type="radio" defaultChecked name="policyTypeRadio" onClick={ () => this.showHomePolicies() }/> Show Home Policies</label>
-        <br />
-        <label><input type="radio" name="policyTypeRadio" onClick={ () => this.showAutoPolicies() }/> Show Auto Policies</label>
+        <label><input type="radio" defaultChecked name="policyTypeRadio" onClick={() => this.showHomePolicies()}/> Show
+          Home Policies</label>
+        <br/>
+        <label><input type="radio" name="policyTypeRadio" onClick={() => this.showAutoPolicies()}/> Show Auto
+          Policies</label>
         <h3>Policies: </h3>
         <div className="policy-list">
-          <Policy policies={policies} policyType={policyType} policyHolders={policyHolders} />
+          <Policy policies={policies} policyType={policyType} policyHolders={policyHolders}/>
         </div>
       </div>
     );
